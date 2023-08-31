a586977d3f535002e00ce2d29a7cdd0fffb25c38
rustytrooper <diansolo40@gmail.com>
2023-09-01 02:00:50 +0300

    init: start doing task but need to fix rounding half

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,34 @@
 class GuessingGame {
-    constructor() {}
+    constructor(number) {
+        this.number = number;
+      }
+      setRange(min, max) {
+        this.min = min;
+        this.max = max;
+      }
+      guess() {
+        this.half = Math.round((this.min + this.max) / 2);
+        if (this.half === this.number) {
+          return this.half;
+        } else if (this.number < this.half) {
+          this.lower();
+        } else if (this.number > this.half) {
+          this.greater();
+        }
+        return this.half;
+      }
+      lower() {
+        this.max = this.half - 1;
+        this.guess();
+        return this.number;
+      }
+      greater() {
+        this.min = this.half + 1;
+        this.guess();
+        return this.number;
+      }
 
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+   
 }
 
 module.exports = GuessingGame;
