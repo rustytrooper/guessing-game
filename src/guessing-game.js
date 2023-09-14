562af7a12033789022f3cbca88da2a56d35889f4
rustytrooper <diansolo40@gmail.com>
2023-09-15 01:56:34 +0300

    refactor: change code to pass tests

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,34 +1,19 @@
 class GuessingGame {
-    constructor(number) {
-        this.number = number;
-      }
-      setRange(min, max) {
-        this.min = min;
-        this.max = max;
-      }
-      guess() {
-        this.half = Math.round((this.min + this.max) / 2);
-        if (this.half === this.number) {
-          return this.half;
-        } else if (this.number < this.half) {
-          this.lower();
-        } else if (this.number > this.half) {
-          this.greater();
-        }
-        return this.half;
-      }
-      lower() {
-        this.max = this.half - 1;
-        this.guess();
-        return this.number;
-      }
-      greater() {
-        this.min = this.half + 1;
-        this.guess();
-        return this.number;
-      }
-
-   
+  constructor() {}
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
+  guess() {
+    this.half = Math.ceil((this.min + this.max) / 2);
+    return this.half;
+  }
+  lower() {
+    this.max = this.half;
+  }
+  greater() {
+    this.min = this.half;
+  }
 }
 
 module.exports = GuessingGame;
